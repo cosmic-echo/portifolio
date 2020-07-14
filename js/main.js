2a5356a629b19094f2d33df9c17736cceedef5b7
Raul Gomes <42724720+cosmic-echo@users.noreply.github.com>
2020-07-13 22:36:33 -0300

    alterações gerais no código e introdução da área de 'orçamentos'

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+
+/*hamburguer menu action*/
 var hamburguer = document.querySelector("#hamburguer");
 
 hamburguer.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("show-menu");
 });
+
+/*transitions only after fully loading the page*/
