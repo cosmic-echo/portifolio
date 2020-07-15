674a27921ab2a52d218706a5342884fb39a5c7fd
Raul Gomes <42724720+cosmic-echo@users.noreply.github.com>
2020-07-15 14:53:29 -0300

    alteração nos formulários

--- js/main.js
+++ js/main.js
@@ -6,16 +6,21 @@ hamburguer.addEventListener("click", function() {
   document.querySelector("#sidebar").classList.toggle("show-menu");
 });
 
-var radio1 = document.querySelector("#radio--request");
-var radio2 = document.querySelector("#radio--contact");
+var radiorequest = document.querySelector("#radio--request");
+var radiocontact = document.querySelector("#radio--contact");
 
-function showform() { 
-  if(radio1.checked) { 
-      document.querySelector("#form--request").classList.toggle("show-form");
-      document.querySelector("#form--contact").classList.remove("show-form");
-  }
-  if(radio2.checked) { 
-      document.querySelector("#form--contact").classList.toggle("show-form");
-      document.querySelector("#form--request").classList.remove("show-form");
-  }
-};
+var formrequest = document.querySelector("#form--request");
+var formcontact = document.querySelector("#form--contact");
+
+radiorequest.addEventListener('click', modifyVisibility, false);
+radiocontact.addEventListener('click', modifyVisibility1, false);
+
+function modifyVisibility() {
+  formrequest.style.display = "block";
+  formcontact.style.display = "none";
+}
+
+function modifyVisibility1() {
+  formrequest.style.display = "none";
+  formcontact.style.display = "display";
+}
