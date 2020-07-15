ba5e9daa4c4ebd8e78fa74e90227d2ccf83c85fd
Raul Gomes <42724720+cosmic-echo@users.noreply.github.com>
2020-07-15 14:37:21 -0300

    CSS agora está separado por seções; formulário de contato e orçamento reformulado

--- js/main.js
+++ js/main.js
@@ -2,8 +2,20 @@
 /*hamburguer menu action*/
 var hamburguer = document.querySelector("#hamburguer");
 
-hamburguer.addEventListener("click", function () {
+hamburguer.addEventListener("click", function() {
   document.querySelector("#sidebar").classList.toggle("show-menu");
 });
 
-/*transitions only after fully loading the page*/
+var radio1 = document.querySelector("#radio--request");
+var radio2 = document.querySelector("#radio--contact");
+
+function showform() { 
+  if(radio1.checked) { 
+      document.querySelector("#form--request").classList.toggle("show-form");
+      document.querySelector("#form--contact").classList.remove("show-form");
+  }
+  if(radio2.checked) { 
+      document.querySelector("#form--contact").classList.toggle("show-form");
+      document.querySelector("#form--request").classList.remove("show-form");
+  }
+};
