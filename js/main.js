6286a34d3f0bf94dd6263a598366bc2b6a87a9c9
Raul Gomes <42724720+cosmic-echo@users.noreply.github.com>
2020-07-20 22:14:13 -0300

    formulários agora possuem inputs com labels dinâmicos

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ hamburguer.addEventListener("click", function() {
   document.querySelector("#sidebar").classList.toggle("show-menu");
 });
 
-var radiorequest = document.querySelector("#radio--request");
-var radiocontact = document.querySelector("#radio--contact");
+var radiorequest = document.querySelector("#radio1--request");
+var radiocontact = document.querySelector("#radio2--contact");
 
-var formrequest = document.querySelector("#form--request");
-var formcontact = document.querySelector("#form--contact");
+var formrequest = document.querySelector("#form1--request");
+var formcontact = document.querySelector("#form2--contact");
 
 radiorequest.addEventListener('click', modifyVisibility, false);
 radiocontact.addEventListener('click', modifyVisibility1, false);
