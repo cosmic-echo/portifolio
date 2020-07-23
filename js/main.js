32c53ff86ed995617238205c7e942c009c0abcee
Raul <42724720+cosmic-echo@users.noreply.github.com>
2020-07-23 20:07:04 -0300

    formulário de contato agora é um popup

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 /*hamburguer menu action*/
 var hamburguer = document.querySelector("#hamburguer");
 
@@ -6,21 +5,32 @@ hamburguer.addEventListener("click", function() {
   document.querySelector("#sidebar").classList.toggle("show-menu");
 });
 
-var radiorequest = document.querySelector("#radio1--request");
-var radiocontact = document.querySelector("#radio2--contact");
+/*Contact popup*/
+var contactpopup = document.querySelector("#contactpopup");
+var contactbutton = document.querySelector("#closecontact");
+var closebutton = document.querySelector("#closecontact");
+var lockpage = document.querySelector("#lockpage");
 
+function showcontactform() {
+  contactpopup.classList.toggle("show-contactform");
+  lockpage.classList.toggle("showlockpage");
+}
+function closecontactform() {
+  contactpopup.classList.remove("show-contactform");
+  lockpage.classList.remove("showlockpage");
+}
+
+/*Switch between contact forms*/
 var formrequest = document.querySelector("#form1--request");
 var formcontact = document.querySelector("#form2--contact");
 
-radiorequest.addEventListener('click', modifyVisibility, false);
-radiocontact.addEventListener('click', modifyVisibility1, false);
 
-function modifyVisibility() {
+function switchrequestform() {
   formrequest.classList.toggle("show-form");
   formcontact.classList.remove("show-form");
 }
 
-function modifyVisibility1() {
+function switchcontactform() {
   formrequest.classList.remove("show-form");
   formcontact.classList.toggle("show-form");
 }
